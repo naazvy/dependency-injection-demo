e4aa03a29736b17edc2e76e56e29a57212091f59
naazvy <sandynazarov@gmail.com>
2019-04-07 21:53:14 +0200

    add @Primary annotation to class

--- src/main/java/com/springfremework/dependencyinjectiondemo/services/PrimaryGreetingService.java
+++ src/main/java/com/springfremework/dependencyinjectiondemo/services/PrimaryGreetingService.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
 @Service
-@Primary
+
 public class PrimaryGreetingService implements GreetingService {
 
     @Override
